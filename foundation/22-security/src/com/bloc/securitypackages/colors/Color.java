5aa42e5a6705166efedcfd76248a31dd33c79086
Steven Nguyen <ngynstvn@gmail.com>
2015-06-17 16:22:17 -0700

    .

---
+++ foundation/22-security/src/com/bloc/securitypackages/colors/Color.java
@@ -0,0 +1,29 @@
+package com.bloc.securitypackages.colors;
+
+/************************************************
+ *	YOU MAY MODIFY THIS FILE AND/OR ITS LOCATION
+/************************************************/
+
+public class Color extends Object {
+	// Name of the color
+	protected String mName;
+	// Alpha value
+	protected int mAlpha;
+	// Red value
+	protected int mRed;
+	// Green value
+	protected int mGreen;
+	// Blue value
+	protected int mBlue;
+
+	Color(int red, int green, int blue) {
+		this(null, red, green, blue);
+	}
+
+	Color(String name, int red, int green, int blue) {
+		mName = name;
+		mRed = red;
+		mGreen = green;
+		mBlue = blue;
+	}
+}
